80355dfac5bc064d9988784efbfed003d3e0efa5
as <>
2021-04-06 05:23:19 +0900

    飛行機をリセット.csで元の位置に戻す

---
+++ Assets/MyScript/ResetPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    Vector3 originalPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("AEROPLANEWALL"))
+        {
+            transform.localPosition = originalPos;
+        }
+    }
+}
